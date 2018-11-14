ff4e90b7c0392810c23584ca032dc3bb872cbd98
chullmir <paulochullmir@gmail.com>
2018-11-14 09:19:16 -0300

    cambios en la pagina de producto

--- views/producto.view.php
+++ views/producto.view.php
@@ -1,6 +1,6 @@
 <body>
   <div class="container producto">
-    <div class="row producto">
+    <div class="row productos">
       <div class="col-6 imagen">
         <div class="imagen_producto">
           <img src="img/producto/producto.jpg" alt="">
@@ -11,14 +11,15 @@
           <p class="codigo">341009</p>
           <p class="precio">$ 50</p>
           <p class="stock">Disponible</p>
-          <div class="opcional">
-            <p class="inflado">Inflado con aire</p>
-            <input type="checkbox" name="" value="">
+          <div class="opcional form-group form-check">
+            <input type="checkbox" class="form-check-input" id="inflado">
+            <label for="inflado">Inflado con aire</label>
           </div>
-          <div class="cantidad">
-            <h3 class="titulo">Cantidad</h3>
-            <input type="number" name="" value="">
+          <div class="cantidad form-group">
+            <label for="cantidad">Cantidad</label>
+            <input type="number" class="form-control" id="cantidad" name="" value="">
           </div>
+          <a href="#" class="btn btn-info">Agregar al Carrito</a>
       </div>
 
 
